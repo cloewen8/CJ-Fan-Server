4b600f46b63567d57c9edbcc2fb80194b90e0739
Colton Loewen <loewencjay@gmail.com>
2018-08-10 00:55:08 -0600

    Implemented placeholder http server
    In the future I would like to switch to a worker dyno, which will not
    provide a port.

--- src/cj-fan-server.cr
+++ src/cj-fan-server.cr
@@ -12,3 +12,20 @@ rescue exc
 	CjFanServer::LOG.fatal("Unable to start.")
 	CjFanServer::LOG.fatal(exc)
 end
+
+
+port = 8080
+
+OptionParser.parse! do |opts|
+	opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+		port = opt.to_i
+	end
+end
+
+server = HTTP::Server.new do |context|
+	context.response.content_type = "text/plain"
+	context.response.print "Hello world! The time is #{Time.now}"
+end
+
+address = server.bind_tcp port
+server.listen
