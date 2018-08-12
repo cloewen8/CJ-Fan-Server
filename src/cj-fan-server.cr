d108121e5942083e5cc99086af98cb84beea82a3
Colton Loewen <loewencjay@gmail.com>
2018-08-11 23:52:35 -0600

    Removed http server

--- src/cj-fan-server.cr
+++ src/cj-fan-server.cr
@@ -1,22 +1,3 @@
-require "option_parser"
-
-port = 8080
-
-OptionParser.parse! do |opts|
-	opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
-		port = opt.to_i
-	end
-end
-
-server = HTTP::Server.new do |context|
-	context.response.content_type = "text/plain"
-	context.response.print "Hello world! The time is #{Time.now}"
-end
-
-address = server.bind_tcp port
-server.listen
-
-
 require "./cj-fan-server/config"
 require "./cj-fan-server/bot"
 
